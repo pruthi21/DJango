6bed525ce0d7ad10f2be28afa1d3731279cd385e
Pruthika wankar <pruthikaw@gmail.com>
2024-08-09 20:35:39 +0530

    css

--- car/car/settings.py
+++ car/car/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR =Path(BASE_DIR,'static')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[STATIC_DIR]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
