bbc331cf088268ec3260e9775241e5fbd546c474
Pruthika wankar <pruthikaw@gmail.com>
2024-08-16 20:40:12 +0530

    app

--- orm/core/views.py
+++ orm/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
+def index(request):
+    hero = Student.objects.all()
+    return render(request, 'core/index.html',{'hero': hero})
